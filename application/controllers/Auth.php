a9f35ab439ffba2a7eadec779a194042a1d2530b
Putu Rama Bena Putra <ramabena.rb@gmail.com>
2020-04-22 14:57:19 +0800

    create database user and make registration feature work

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,17 +3,56 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
     public function index()
     {
-        $this->load->view('templates/auth_header');
+        $data['title'] = 'PO User Login';
+
+        $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
     }
 
     public function registration()
     {
-        $this->load->view('templates/auth_header');
-        $this->load->view('auth/registration');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('name', 'Name', 'required|trim', [
+            'required' => 'nama tidak boleh kosong'
+        ]);
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'required' => 'email tidak boleh kosong',
+            'valid_email' => 'email tidak valid',
+            'is_unique' => 'email sudah terdaftar'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'required' => 'password tidak boleh kosong',
+            'matches' => 'Password tidak sama',
+            'min_length' => 'Password terlalu pendek'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'PO User Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $data = [
+                'name' => $this->input->post('name'),
+                'email' => $this->input->post('email'),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT), // kayaknya password1 yg bener
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Registrasi Berhasil. Silahkan login untuk masuk</div>');
+            redirect('auth');
+        }
     }
 }
--- application/views/templates/auth_header.php
+++ application/views/templates/auth_header.php
@@ -8,8 +8,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
-    <title>SB Admin 2 - Login</title>
+    <title>
+        <?= $title; ?>
+    </title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
