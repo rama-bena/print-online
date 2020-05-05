5b5fd2f086389ab97f0716a2ed75d2101e7f7bcc
Putu Rama Bena Putra <ramabena.rb@gmail.com>
2020-05-05 22:46:57 +0800

    add order menu

---
+++ application/controllers/Order.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Order extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+
+    public function index()
+    {
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['title'] = 'Print Now';
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('order/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function historyOrder()
+    {
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['title'] = 'History Order';
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('order/historyOrder', $data);
+        $this->load->view('templates/footer');
+    }
+}
