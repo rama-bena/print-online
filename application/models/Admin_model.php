013ded1758a79fcce2035d3b96254875b31c07cf
Putu Rama Bena Putra <ramabena.rb@gmail.com>
2020-05-07 04:08:00 +0800

    delete file on server and database when the user is deleted

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -9,9 +9,40 @@ class Admin_model extends CI_Model
         $this->load->model('General_model', 'model');
         $this->model->deleteImage($user['image']);
 
+        $this->_deleteFileInServer('./file-print/' . $id);
+        $this->_deletePrintInDatabase($id);
         $this->db->where('id', $id);
         $this->db->delete('user');
     }
+
+    private function _deleteFileInServer($target)
+    {
+        if (is_dir($target)) {
+            $files = glob($target . '*', GLOB_MARK); //GLOB_MARK adds a slash to directories returned
+            foreach ($files as $file) {
+                delete_files($file);
+            }
+
+            rmdir($target);
+        } elseif (is_file($target)) {
+            unlink($target);
+        }
+    }
+
+    private function _deletePrintInDatabase($id)
+    {
+        $printFiles = $this->db->get_where('print_file', ['id_user' => $id])->result_array();
+
+        foreach ($printFiles as $printFile) {
+            $idFile = $printFile['id'];
+            $this->db->where('id_file', $idFile);
+            $this->db->delete('print_order');
+        }
+
+        $this->db->where('id_user', $id);
+        $this->db->delete('print_file');
+    }
+
 
     public function changeRole($id, $role_id)
     {
